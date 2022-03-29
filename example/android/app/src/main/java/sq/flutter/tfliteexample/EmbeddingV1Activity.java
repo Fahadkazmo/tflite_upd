bfc0f62e1a8f0bdd86082c4082d5f17eb48a3b53
Fahadkazmo <62807745+Fahadkazmo@users.noreply.github.com>
2022-03-29 10:18:27 +0100

    Add files via upload

---
+++ example/android/app/src/main/java/sq/flutter/tfliteexample/EmbeddingV1Activity.java
@@ -0,0 +1,20 @@
+ package io.flutter.plugins.firebasecoreexample;
+ package sq.flutter.tfliteexample;
+ import android.os.Bundle;
+ import io.flutter.embedding.android.FlutterActivity;
+ import io.flutter.embedding.engine.FlutterEngine;
+ import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+
+  package io.flutter.plugins.firebase.core;
+
+ import androidx.test.rule.ActivityTestRule;
+ import dev.flutter.plugins.e2e.FlutterRunner;
+ import io.flutter.plugins.firebasecoreexample.EmbeddingV1Activity;
+ import org.junit.Rule;
+ import org.junit.runner.RunWith;
+
+ public class MainActivity extends FlutterActivity {
+   // You can keep this empty class or remove it. Plugins on the new embedding
+   // now automatically registers plugins.
+ }
+
